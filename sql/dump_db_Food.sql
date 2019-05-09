4d45b700941ceb5981a3295686a4085f9a86a63e
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-05-08 18:38:46 -0700

    some changes to sql files

--- sql/dump_db_Food.sql
+++ sql/dump_db_Food.sql
@@ -2,4 +2,4 @@
 mysqldump -u foodUser -p Food > C:\Apache24\htdocs\project\dbfood_backup.sql
 /*password = food123 */
 
-in mysql > drop database Food; /*drops the database*/ 
+/* in mysql > drop database Food; --- drops the database*/
