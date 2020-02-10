c7638153a39ceb51eec0248f2919a6bc92448c73
Dallas <dallas.mcgroarty@gmail.com>
2020-02-10 12:42:02 -0800

    Update connect_db.php

--- db/connect_db.php
+++ db/connect_db.php
@@ -3,7 +3,7 @@
 
 function connect_db() {
  
-    $conn = new mysqli("localhost", "foodUser" , "food123", "Food");
+    $conn = new mysqli("host", "user" , "password", "db");
 
     if($conn->connect_error) {
         die("connection failed: " . $conn->connect_error);
