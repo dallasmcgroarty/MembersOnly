672d30f068ea956d3b1c8a16e701802cdf0d395b
Dallas <dallas.mcgroarty@gmail.com>
2020-02-10 12:42:28 -0800

    Delete create_foodUser.sql

--- sql/create_foodUser.sql
+++
@@ -1,5 +0,0 @@
-/*create user script
-  needed to use mysql_native_password otherwise i couldnt connect to the database using mysqli */
-CREATE USER 'foodUser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'food123';
-GRANT ALL ON Food.* TO 'foodUser'@'localhost';
-FLUSH PRIVILEGES;
